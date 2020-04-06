fd751f82f1da52a901661e6f8fea793f23d7ddd9
Arj || Diode <arjunanmr101@gmail.com>
2020-04-06 12:22:05 +0530

    come changes

--- sumifier.test.js
+++ sumifier.test.js
@@ -1,13 +1,21 @@
 
 const functions = require('./sumifier');
 
-
+const testResults = {};
 
 test("Testing if 1 + 2 is 3", () => {
-  expect(functions.sum(1,2)).toBe(3);
+  const test_sum__result = functions.sum(1,2);
+  testResults.sumResult = test_sum__result;
+  expect(test_sum__result).toBe(3);
 })
 
 test("Testing randomizer", () =>{
-  expect(functions.randomizer(5)).not.toBe(5);
+  const test_randomizer__result = functions.randomizer(5);
+  testResults.randomizerResult = test_randomizer__result;
+  expect(test_randomizer__result).toBeGreaterThanOrEqual(0)
+  expect(test_randomizer__result).toBeLessThanOrEqual(10);
+})
 
+afterAll(() => {
+  console.log(testResults);
 })
